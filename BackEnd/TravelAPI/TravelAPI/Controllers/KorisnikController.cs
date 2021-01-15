3e644cac1a4d29e83fd7b6e12c89c660e7fa7a22
aleksandar <acaacaas@gmail.com>
2021-01-15 20:30:20 +0100

    dodavanje mesta

---
+++ BackEnd/TravelAPI/TravelAPI/Controllers/KorisnikController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
+using Neo4jClient.Cypher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelAPI.DomainModel;
+
+namespace TravelAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KorisnikController : ControllerBase
+    {
+        private readonly IGraphClient client;
+
+        public KorisnikController(IGraphClient client)
+        {
+            this.client = client;
+        }
+
+        [HttpGet("{email}")]
+        public Korisnik GetKorisnik(string email)
+        {
+            var query = new CypherQuery("match (k:Korisnik {email:'" + email + "'}) return k",
+                                        new Dictionary<string, object>(), CypherResultMode.Set);
+
+            Korisnik korisnik = ((IRawGraphClient)client).ExecuteGetCypherResults<Korisnik>(query).FirstOrDefault();
+
+            return korisnik;
+        }
+
+        [HttpPost]
+        public IActionResult PostKorisnik(Korisnik korisnik)
+        {
+            Dictionary<string, object> queryDict = new Dictionary<string, object>();
+            queryDict.Add("Ime", korisnik.Ime);
+            queryDict.Add("Prezime", korisnik.Prezime);
+            queryDict.Add("email", korisnik.email);
+            queryDict.Add("password", korisnik.password);
+
+            var query = new CypherQuery("CREATE (k:Korisnik {Ime:'" + korisnik.Ime
+                                        + "', Prezime:'" + korisnik.Prezime
+                                        + "', email:'" + korisnik.email
+                                        + "', password:'" + korisnik.password
+                                        + "'}) return k",
+                                        queryDict, CypherResultMode.Set);
+
+            ((IRawGraphClient)client).ExecuteCypher(query);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public ActionResult<object> Login(Korisnik korisnik)
+        {
+            var query = new CypherQuery("match (k:Korisnik {email:'" + korisnik.email
+                                        + "', password:'" + korisnik.password
+                                        + "'}) return k",
+                                        new Dictionary<string, object>(), CypherResultMode.Set);
+
+            Korisnik korisnik_ret = ((IRawGraphClient)client).ExecuteGetCypherResults<Korisnik>(query).FirstOrDefault();
+
+            if (korisnik_ret != null)
+            {
+                return new
+                {
+                    Uspesno = true,
+                    Ime = korisnik_ret.Ime,
+                    Prezime = korisnik_ret.Prezime
+                };
+            }
+            else
+            {
+                return new
+                {
+                    Uspesno = false
+                };
+            }
+        }
+    }
+}
---
+++ BackEnd/TravelAPI/TravelAPI/Controllers/MestoController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
+using Neo4jClient.Cypher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelAPI.DomainModel;
+
+namespace TravelAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MestoController : ControllerBase
+    {
+        private readonly IGraphClient client;
+
+        public MestoController(IGraphClient client)
+        {
+            this.client = client;
+        }
+
+        [HttpPost]
+        public IActionResult PostMesta(List<Mesto> mesta)
+        {
+            // Dodavanje cvorova
+            foreach (var mesto in mesta)
+            {
+                // Pokusamo da ga nadjemo u bazu
+                var query = new CypherQuery("match (m:Mesto {Naziv:'" + mesto.Naziv + "'}) return m",
+                                        new Dictionary<string, object>(), CypherResultMode.Set);
+
+                Mesto mesto_ret = ((IRawGraphClient)client).ExecuteGetCypherResults<Mesto>(query).FirstOrDefault();
+
+                if (mesto_ret == null)
+                {
+                    // Treba ga dodati u bazu
+
+                    Dictionary<string, object> queryDict = new Dictionary<string, object>();
+                    queryDict.Add("Naziv", mesto.Naziv);
+                    queryDict.Add("Opis", mesto.Opis);
+                    queryDict.Add("Ocena", mesto.Ocena);
+                    queryDict.Add("ImeSlike", mesto.ImeSlike);
+
+                    query = new CypherQuery("CREATE (m:Mesto {Naziv:'" + mesto.Naziv
+                                            + "', Opis:'" + mesto.Opis
+                                            + "', Ocena:" + mesto.Ocena
+                                            + ", ImeSlike:'" + mesto.ImeSlike
+                                            + "'}) return m",
+                                            queryDict, CypherResultMode.Set);
+
+                    ((IRawGraphClient)client).ExecuteCypher(query);
+                }
+            }
+            
+            // Dodavanje grana
+            for (int i = 1; i < mesta.Count; i++)
+            {
+                var query = new CypherQuery("MATCH (a:Mesto), (b:Mesto)" +
+                                            "WHERE a.Naziv= '" + mesta[0].Naziv + "' AND b.Naziv= '" + mesta[i].Naziv + "'" +
+                                            "MERGE (a)-[:NEXT]->(b)",
+                                            new Dictionary<string, object>(), CypherResultMode.Set);
+
+                ((IRawGraphClient)client).ExecuteCypher(query);
+            }
+
+            return Ok();
+        }
+    }
+}
