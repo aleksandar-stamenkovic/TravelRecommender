590fcc34b485ffa7a617e4606c79ac8eee646c5a
mixy <milan.mixy.stankovic@elfak.rs>
2021-01-15 15:09:13 +0100

    model korisnik

---
+++ BackEnd/TravelAPI/TravelAPI/DomainModel/Korisnik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAPI.DomainModel
+{
+    public class Korisnik
+    {
+
+        public string ID { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public string Ime { get; set; }
+
+        public string Prezime { get; set; }
+
+       
+
+    }
+}
