4389c0f1a14624e0f10ed93b44f68e668bff9d97
mixy <milan.mixy.stankovic@elfak.rs>
2021-01-15 14:55:38 +0100

    model Mesto

---
+++ BackEnd/TravelAPI/TravelAPI/DomainModel/Mesto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAPI.DomainModel
+{
+    public class Mesto
+    {
+        public string ID { get; set; }
+
+        public string Naziv { get; set; }
+
+        public string Opis { get; set; }
+
+        public float Ocena { get; set; }
+
+        public string ImeSlike { get; set; }
+
+    }
+}
